80aee1585bafbc60e85c0c4ef0dcffe8a276916f
Nathan Huang <longhaoh1370@gmail.com>
2024-10-18 13:35:04 -0400

    found all bugs

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -18,4 +18,87 @@ describe('Calculator', (): void => {
 
   });
 
+  it('should display `8+2` when `4*2+2` is invoked', (): void => {
+
+    calculator.pressFour();
+    calculator.pressMult();
+    calculator.pressTwo();
+    calculator.pressPlus();
+    calculator.pressTwo();
+    const displayValue: string = calculator.display();
+
+    expect(displayValue).toEqual('8+2');
+
+  });
+
+  it('should display `3` when `1.+2=` is invoked', (): void => {
+
+    calculator.pressOne();
+    calculator.pressDot();
+    calculator.pressPlus();
+    calculator.pressTwo();
+    calculator.pressEquals();
+    const displayValue: string = calculator.display();
+
+    expect(displayValue).toEqual('3');
+
+  });
+
+  it('should display `4/` when `8/2/` is invoked', (): void => {
+
+    calculator.pressEight();
+    calculator.pressDiv();
+    calculator.pressTwo();
+    calculator.pressDiv();
+    const displayValue: string = calculator.display();
+
+    expect(displayValue).toEqual('4/');
+
+  });
+
+  it('should display `3` when `-1+4=` is invoked', (): void => {
+
+    calculator.pressMinus();
+    calculator.pressOne();
+    calculator.pressPlus();
+    calculator.pressFour();
+    calculator.pressEquals();
+    const displayValue: string = calculator.display();
+
+    expect(displayValue).toEqual('3');
+
+  });
+
+  it('should display `-2+` when `-4/2+` is invoked', (): void => {
+
+    calculator.pressMinus();
+    calculator.pressFour();
+    calculator.pressDiv();
+    calculator.pressTwo();
+    calculator.pressPlus();
+    const displayValue: string = calculator.display();
+
+    expect(displayValue).toEqual('-2+');
+
+  });
+
+  it('should display `22` when `10*2+20/10` is invoked', (): void => {
+
+    calculator.pressOne();
+    calculator.pressZero();
+    calculator.pressMult();
+    calculator.pressTwo();
+    calculator.pressPlus();
+    calculator.pressTwo();
+    calculator.pressZero();
+    calculator.pressDiv();
+    calculator.pressOne();
+    calculator.pressZero();
+    calculator.pressEquals();
+    const displayValue: string = calculator.display();
+
+    expect(displayValue).toEqual('22');
+
+  });
+
 });
